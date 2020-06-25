6b5289b75338a1150d259003ccd0e17c59015fce
Stian have <stianhave@hotmail.com>
2020-06-25 08:59:27 +0200

    Refactored into shared module and removed unused imports

--- APM/src/app/products/product.module.ts
+++ APM/src/app/products/product.module.ts
@@ -1,23 +1,18 @@
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductListComponent } from './product-list.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { StarComponent } from '../shared/star.component';
 import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
     ConvertToSpacesPipe,
-    StarComponent,
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       {
@@ -26,6 +21,7 @@ import { ProductDetailGuard } from './product-detail.guard';
         component: ProductDetailComponent,
       },
     ]),
+    SharedModule,
   ],
 })
 export class ProductModule {}
